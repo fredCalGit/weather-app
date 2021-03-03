00579e10230e6107d47c7f1e8ffebddf8ede417f
Fred Cal <fred.cal@hotmail.com>
2021-03-02 21:54:36 -0300

    Fixed wrong postman request

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,4 +1,4 @@
-const request = require('postman-request')
+const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?limit=1&access_token=pk.eyJ1IjoiZnJlZGNhbCIsImEiOiJja2xnZHptMzMwZnVnMnlzNzlpcXduZGt1In0.16AuH4QbVxEwKZQtWV8e6w'
